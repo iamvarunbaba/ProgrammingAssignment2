dd5ea958b2af2e40c3350d94a8f498e3bb5774c7
iamvarunbaba <varun.ramakrishnan@gmail.com>
2015-03-23 00:48:50 +0530

    Unit Testing completed
    Unit testing for the Caching has been completed, and works as expected

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,10 @@
 ## User ID          : 5076930
 ## Submission Login : varun.ramakrishnan@gmail.com
 
-## cachematrix.r Stub has been forked from https://github.com/rdpeng/ProgrammingAssignment2
-
 ## Matrix inversion is usually a costly computation and there may be some benefit to caching the inverse of a matrix rather than compute it repeatedly. 
 ## The following two functions are used to cache the inverse of a matrix.
 
-
-# This is the "makeCacheMatrix" Function, It creates a list containing a function to
+# Below is the "makeCacheMatrix" Function, It creates a list containing a function to
 #1. set the value of the matrix
 #2. get the value of the matrix
 #3. set the value of inverse of the matrix
@@ -28,13 +25,6 @@ get <- function() x                  # returns the input matrix
 setInv <- function(inv) xinv <<- inv # sets the inversed matrix
 getInv <- function() xinv            # returns the inversed matrix
       
-# Returns a list that contains these functions, so that we can use makeCacheMatrix object like these
-# x <- makeCacheMatrix(testmatrix)
-# x$set(newmatrix) # to change matrix
-# x$get            # to get the setted matrix
-# x$setInv         # to set the inversed matrix
-# x$getInv         # to get the inversed matrix
-      
 list(set = set, 
 	 get = get,
 	 setInv = setInv,
@@ -42,7 +32,7 @@ list(set = set,
 		   
 }
 
-# The "cacheSolve" function returns the inverse of the matrix. 
+# Below is the "cacheSolve" function which returns the inverse of the matrix. 
 # If the inverse has already been computed, it gets the result and skips the computation. 
 # If not, it computes the inverse and sets the value in the cache.
 # (Assumption :- The matrix is always invertible.)
@@ -66,4 +56,4 @@ cacheSolve <- function(x, ...) {
 		
 }
 
-
+## "cachematrix.r" Stub has been forked from https://github.com/rdpeng/ProgrammingAssignment2
